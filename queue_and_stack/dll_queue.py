f3db1397307f414ae29654723f6c52a1de2bec63
Dave Vazquez <dmvaz.li@gmail.com>
2020-03-23 17:46:47 -0400

    dll_queue.py solution

--- queue_and_stack/dll_queue.py
+++ queue_and_stack/dll_queue.py
@@ -1,19 +1,35 @@
-import sys
-sys.path.append('../doubly_linked_list')
 from doubly_linked_list import DoublyLinkedList
+import sys
+import os
+sys.path.append('../doubly_linked_list')
+os.system('clear')
+
+
+'''
+### Queues∫
+ * Should have the methods: `enqueue`, `dequeue`, and `len`.
+   * `enqueue` should add an item to the back of the queue.
+   * `dequeue` should remove and return an item from the front of the queue.
+   * `len` returns the number of items in the queue.
+ 
+![Image of Queue](https://upload.wikimedia.org/wikipedia/commons/thumb/5/52/Data_Queue.svg/600px-Data_Queue.svg.png)
+'''
 
 
 class Queue:
     def __init__(self):
-        self.size = 0
-        # Why is our DLL a good choice to store our elements?
-        # self.storage = ?
+        self.storage = DoublyLinkedList()
+        self.size = len(self.storage)
 
     def enqueue(self, value):
-        pass
+        self.storage.add_to_tail(value)
+        self.size = len(self.storage)
 
     def dequeue(self):
-        pass
+        if len(self.storage) > 0:
+            removed = self.storage.remove_from_head()
+            self.size = len(self.storage)
+            return removed
 
     def len(self):
-        pass
+        return len(self.storage)
